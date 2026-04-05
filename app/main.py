fac3a2f671baf193c9119249b2d86d60d95d838e
ravindu374 <rlakshan374@gmail.com>
2026-04-05 20:33:57 +0530

    fix cors + correct api url

--- app/main.py
+++ app/main.py
@@ -11,9 +11,7 @@ app = FastAPI(title="EngineerFlow API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",  # frontend
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
